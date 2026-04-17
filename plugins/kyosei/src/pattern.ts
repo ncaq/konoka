3e1578936e452d250b0214ae51bf3aa4eb4d2708
ncaq <ncaq@ncaq.net>
2026-04-17 13:31:31 +0900

    feat: リモートURLのパターンを追加

--- plugins/kyosei/src/pattern.ts
+++ plugins/kyosei/src/pattern.ts
@@ -8,3 +8,20 @@
  * 末尾のサブパス(/files, /commits等)やクエリパラメータは無視します。
  */
 export const prUrlPattern: RegExp = /^https?:\/\/([^/]+)\/([^/]+)\/([^/]+)\/pull\/(\d+)/;
+
+/**
+ * Gitリモートの各種URL形式にマッチするパターン。
+ * HTTPS, SSH, git+ssh形式に対応しています。
+ *
+ * マッチする形式:
+ * - `https://github.com/owner/repo.git`
+ * - `https://github.com/owner/repo`
+ * - `git@github.com:owner/repo.git`
+ * - `git@github.com:owner/repo`
+ * - `ssh://git@github.com/owner/repo.git`
+ */
+export const remoteUrlPatterns: readonly RegExp[] = [
+  /^https?:\/\/[^/]+\/([^/]+)\/([^/]+?)(?:\.git)?\/?$/,
+  /^git@[^:]+:([^/]+)\/([^/]+?)(?:\.git)?\/?$/,
+  /^ssh:\/\/[^/]+\/([^/]+)\/([^/]+?)(?:\.git)?\/?$/,
+];
