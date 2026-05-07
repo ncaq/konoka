a43e72ee293bcbf90fe18e2f117cdc0a03665851
ncaq <ncaq@ncaq.net>
2026-05-07 18:07:39 +0900

    fix: `linkedProjects`の値をベクター記法に変更してLSPに反映させる
    `lsp-rust-analyzer-linked-projects`の型は`lsp-string-vector`であり、
    コンスリスト`(...)`で渡すとJSON配列として正しくシリアライズされず、
    rust-analyzerが`linkedProjects`を認識しませんでした。
    ベクター記法`[...]`に変更したことでJSON配列として送信され、
    ワークスペース発見が機能するようになります。

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
 ((nil . ((lsp-format-buffer-on-save . t)))
- (rustic-mode . ((lsp-rust-analyzer-linked-projects . ("plugins/rm-to-trash/Cargo.toml")))))
+ (rustic-mode . ((lsp-rust-analyzer-linked-projects . ["plugins/rm-to-trash/Cargo.toml"]))))
