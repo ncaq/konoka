06be01fb47d84919c1a7e7a2d0e0fd0a9ad2121b
ncaq <ncaq@ncaq.net>
2026-05-07 20:03:46 +0900

    docs(input): `read_hook_input`のコメントを修正
    修正漏れ。

--- plugins/rm-to-trash/src/input.rs
+++ plugins/rm-to-trash/src/input.rs
@@ -2,7 +2,6 @@ use crate::serialize::{HookInput, decode_hook_input};
 use std::io::{self, Read};
 
 /// 標準入力からデータを読み込みデコードして返す。
-/// ランタイムなエラーが発生した場合はパニックしてエラーメッセージを出力する。
 pub fn read_hook_input() -> Result<HookInput, Box<dyn std::error::Error>> {
     read_hook_input_from(io::stdin().lock())
 }
