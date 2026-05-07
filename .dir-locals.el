6eb61529db2bb0004b542aab290e5eba9f3876a4
ncaq <ncaq@ncaq.net>
2026-05-07 18:11:01 +0900

    style: .dir-locals.elのインデントを修正

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
 ((nil . ((lsp-format-buffer-on-save . t)))
- (rustic-mode . ((lsp-rust-analyzer-linked-projects . ["plugins/rm-to-trash/Cargo.toml"]))))
+ (rustic-mode
+  . ((lsp-rust-analyzer-linked-projects
+      . ["plugins/rm-to-trash/Cargo.toml"]))))
