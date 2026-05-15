a03e4a500f7faeb6b7bb72f0521fdfce28c9e989
ncaq <ncaq@ncaq.net>
2026-05-16 08:01:27 +0900

    docs: flake.nix内のcargo-fmtチェックコメントを改善
    treefmtのrustfmtは行わなくなったのに、
    ドキュメントがズレていました。

--- flake.nix
+++ flake.nix
@@ -123,7 +123,8 @@
                   extraInputs = with pkgs; [ clippy ];
                 }
                 {
-                  # treefmtでも整形チェックは行うが、cargo純正のフォーマッタチェックも一応実施する。
+                  # treefmtでも整形チェックは行えるが、
+                  # プロジェクト固有の設定をこちらのほうが読み込みやすい。
                   name = "cargo-fmt";
                   script = "cargo fmt --all --check";
                   extraInputs = with pkgs; [ rustfmt ];
