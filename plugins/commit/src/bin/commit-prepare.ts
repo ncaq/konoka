215e36526f721bd3002773e44d1a24a95e3780c9
ncaq <ncaq@ncaq.net>
2026-05-20 12:28:51 +0900

    refactor(commit): 未使用のExitインポートを削除

--- plugins/commit/src/bin/commit-prepare.ts
+++ plugins/commit/src/bin/commit-prepare.ts
@@ -1,5 +1,5 @@
 import { NodeContext, NodeRuntime } from "@effect/platform-node";
-import { Console, Effect, Exit } from "effect";
+import { Console, Effect } from "effect";
 import { prepareCommit } from "../commit-prepare";
 
 const program = prepareCommit.pipe(
